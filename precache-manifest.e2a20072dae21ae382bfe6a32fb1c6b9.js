47d2a8111087e2b9e103e6cc976d683fce056b29
roldie9 <aroldoreyes9@gmail.com>
2020-04-30 22:28:41 -0500

    Updates

---
+++ precache-manifest.e2a20072dae21ae382bfe6a32fb1c6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8aa27d9b0247babd44b30228fe21d5",
+    "url": "/React-Employee-Directory-HW19/index.html"
+  },
+  {
+    "revision": "fdac2a0d5efe99ad10fa",
+    "url": "/React-Employee-Directory-HW19/static/css/main.c84cf82e.chunk.css"
+  },
+  {
+    "revision": "2d237f32092f27538683",
+    "url": "/React-Employee-Directory-HW19/static/js/2.8e72fcd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory-HW19/static/js/2.8e72fcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdac2a0d5efe99ad10fa",
+    "url": "/React-Employee-Directory-HW19/static/js/main.ba643abc.chunk.js"
+  },
+  {
+    "revision": "be9e34b2feea1241a69d",
+    "url": "/React-Employee-Directory-HW19/static/js/runtime-main.8d927039.js"
+  }
+]);
